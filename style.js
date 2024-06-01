0a48663cab2f2a5bb6a92d1637f8d09f1d625dd3
Patrandler <166053055+Patrandler@users.noreply.github.com>
2024-06-01 23:38:00 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,9 +1,32 @@
-const cost1 = ["Raupi", "Hornliu", "Rattfratz", "Paras"];
-const cost2 = ["Taubsi", "Habitak", "Rettan", "Sandan", "Zubat", "Myrapla", "Bluzuk", "Enton", "Krabby", /*"Voltoball",*/ "Ditto"];
-const cost3 = ["Bisasam", "Schiggy", "Glumanda", "Nidoran W", "Nidoran M", "Vulpix", "Digda", "Quapsel", "Abra", "Machollo", /*Knofensa*/ "Tentacha", /*"Kleinstein"*/ "Ponita", "Flegmon", "Magnetilo", "Jurob", "Sleima", "Nebulak", "Traumato", "Tragosso", "Smogon", "Rihorn", /*"Tangela"*/ "Goldini", /*"Amonitas"*/ /*"Kabuto"*/];
-const cost4 = ["Pikachu", /*"Pipi"*/ "Pummeluff", "Mauzi", /*"Menki"*/ "Fukano", "Porenta", "Dodu", "Onix", /*"Owei"*/ "Seeper", /*"Sterndu"*/ /*"Pantimos"*/ "Rossana", "Pinsir", "Evoli", /*"Porygon"*/ "Dratini"];
-const cost5 = [/*"Nockchan", "Kicklee", "Schlurp","Chaneira", "Kangama"*/ "Sichlor", /*"Elektek"*/ "Magmar", "Tauros", "Lapras", /*"Aerodactyl",*/ "Relaxo"]
-const cost6 = [/*"Arktos", "Zapdos", "Lavados",*/ "Mew"]
+const cost1 = ["Raupi", "Hornliu", "Rattfratz", "Paras", "Wiesor", "Hoothoot", "Ledyba", "Webarak", "Hoppspross", "Sonnkern", "Icognito"
+, "Waumpel", "Zirpurze", "Burmi", "Purmel", "Tarundel"];
+const cost2 = ["Taubsi", "Habitak", "Rettan", "Sandan", "Zubat", "Myrapla", "Bluzuk", "Enton", "Krabby", "Ditto", "Natu", "Felino", "Tannza"
+, "Fiffyen", "Zigzachs", "Gehweiher", "Plusle", "Minun", "Pandir", "Illumise", "Liebiskus"
+, "Wadribie", "Bidiza", "Nagelotz", "Dusselgurr", "Scoppel", "Mangunior", "Mabula", "AloRattfratz"
+, "Raffel", "Sensect", "Ferkuli" ];
+const cost3 = ["Bisasam", "Schiggy", "Glumanda", "Nidoran W", "Nidoran M", "Vulpix", "Digda", "Quapsel", "Abra", "Machollo", "Tentacha",  "Ponita", "Flegmon", "Magnetilo", "Jurob", "Sleima", "Nebulak", "Traumato", "Tragosso", "Smogon", "Rihorn", "Goldini", "Endivie", "Feurigel", "Karnimani", "Lampi", "Pichu", "Pii", "Fluffeluff", "Voltilamm", "Griffel", "Traunfugil", "Snubbull", "Baldorfish", "Phanpy", "Farbeagle", "Kussilla"
+, "Geckarbor", "Flemmli", "Hydropi", "Loturzel", "Samurzel", "Schwalbini", "Wingull", "Knilz", "Flurmel", "Makuhita", "Azurill", "Nasgnet", "Eneco", "Zobiris", "Stollunior", "Frizelbliz", "Schluppuck", "Kanivanha", "Wailmer", "Camaub", "Spoink", "Tuska", "Wablu", "Schmerbe", "Krebscorps", "Puppance", "Liliep", "Anorith", "Shuppet", "Isso", "Schneppke", "Perlu"
+, "Chelast", "Panflam", "Plinfa", "Staralili", "Sheinux", "Knospi", "Koknodon", "Schilterus", "Bamelin", "Kikugi", "Schalellos", "Driftlon", "Charmian", "Klingplim", "Skunkapuh", "Bronzel", "Pantimimi", "Hippopotas", "Pionskora", "Glibunkel", "Finneon", "Mantirps"
+, "Serpifeu", "Floink", "Ottaro", "Yorkleff", "Felilou", "Vegimak", "Grillmak", "Sodamak", "Somniam", "Elezeba", "Kiesling", "Fleknoil", "Praktibalk", "Schallquap", "Strawickl", "Toxiped", "Waumboll", "Lilminip", "Ganovil", "Lithomith", "Zurrokex", "Makabaja", "Unratütox", "Zorua", "Picochilla", "Mollimorba", "Monozyto", "Piccolente", "Gelatini", "Sesokitz", "Emolga", "Tarnpignon", "Quabbel", "Wattzapf", "Kastadur", "Klikk", "Zapplardin", "Petznief", "Lin-Fu", "Golbit", "Geronimatz"
+, "Igamaro", "Fynx", "Froxy", "Dartiri", "Leufeo", "Flabébé", "Pam-Pam", "Parfi", "Flauschling", "Iscalar", "Bithora", "Scampisto", "Eguana", "Amarino", "Irrbis", "Arktip"
+, "Bauz", "Flamiau", "Robball", "Peppeck", "Wommel", "Wuffels", "Lusardin", "Garstella", "Pampuli", "Araqua", "Imantis", "Molunk", "Frubberl", "Reißlaus", "Sankabuh", "Gufa", "AloKleinstein", "AloSleima"
+, "Chimpep", "Hopplo", "Memmeon", "Kleptifux", "Cottini", "Wolly", "Kamehaps", "Voldi", "Klonkett", "Urgel", "Pikuda", "Klopptopus", "Fatalitee", "Bähmon", "Hokumil", "Britzigel", "Morpeko"
+, "Felori", "Krokel", "Kwaks", "Micrick", "Pamo", "Hefel", "Krawalloro", "Voltrel", "Sproxi", "Tentagra", "Chilingel", "Relluk", "Flattutu", "Schligda", "Adebom", "PalFelino"
+];
+const cost4 = ["Pikachu", "Pummeluff", "Mauzi",  "Fukano", "Porenta", "Dodu", "Onix",  "Seeper",   "Rossana", "Pinsir", "Evoli",  "Dratini", "Marill", "Kramurx", "Dummisel", "Girafarig", "Skorgla", "Teddiursa", "Damhirplex", "Elekid", "Magby", "Larvitar"
+, "Bummelz", "Nincada", "Meditie", "Roselia", "Qurtel", "Knacklion", "Sonnfel", "Barschwa", "Kecleon", "Palimpalim", "Relicanth", "Kindwurm"
+, "Mobai", "Mampfaxo", "Kaumalat"
+, "Kryppuk", "Rotom", "Rotomurf", "Ohrdoch", "Flampion", "Galapaflos", "Flapteryx", "Milza", "Flunschlik", "Gladiantri", "Kapuno"
+, "Coiffwaff", "Gramokles", "Dedenne", "Clavion", "eF-eM", "Krabbox", "AloSandan", "AloVulpix"
+, "Meikro", "Knapfel", "Kubuin", "Kupfanti", "GalMauzi", "GalPonita", "GalCorason", "GalFlunschliko"
+, "Geosali", "Forgita", "Normifin", "Knattox", "Schlurm", "Lumispross", "Gruff", "Flaminkno", "Agiluza", "Mortcha"
+
+];
+const cost5 = ["Sichlor", "Magmar", "Tauros", "Lapras", "Relaxo", "Skaraborn", "Tropius", "Symvolara", "Shardrago", "Bisofank", "Furnifraß"
+, "Quartermak", "Koalelu", "Mimigma", "Sen-Long", "Moruda", "Duraludon", "GalPorenta"
+, "Mopex", "Nigiragi", "Gierspenst", "PalTauros"
+]
+const cost6 = ["Darkrai", "Kapu-Riki", "Chonjian", "Beatori", "Kriechflügel"]
 const cost8 = ["Mewtwo"]
 
 
@@ -22,8 +45,8 @@ function myFunction() {
 
   while (pointCounter < 10) {
 
-    let firstPick = randomizer(8);
-   /* do {
+    let firstPick = randomizer(30);
+    /*do {
       firstPick = randomizer(8);
       // Check for overflow with all possibilities (assuming pointCounter starts at 0)
     } while ( (firstPick === 1 && pointCounter + 3 > 10) ||
@@ -34,47 +57,50 @@ function myFunction() {
       (firstPick === 6 && pointCounter + 6 > 10) ||
       (firstPick === 7 && pointCounter + 8 > 10)
       // ... add checks for all other options (4, 5, 6, 7)
-      ); */
+      );*/
 
-    if (firstPick === 1) {
+    if (firstPick >= 0 && firstPick <= 5) {
       pointCounter += 3;
       const chosenItem = pickItem(cost3);
       document.getElementById("pokemon").textContent += chosenItem + " (3 Punkte)";
 
-    } else if (firstPick === 2) {
+    } else if (firstPick >= 6 && firstPick <= 11) {
       const chosenItem = pickItem(cost2);
       document.getElementById("pokemon").textContent += chosenItem + " (2 Punkte)";
       pointCounter += 2;
-    } else if (firstPick === 3) {
+    } else if (firstPick >= 12 && firstPick <= 17) {
       const chosenItem = pickItem(cost1);
       document.getElementById("pokemon").textContent += chosenItem + " (1 Punkt)";
       pointCounter += 1;
     }
-    else if (firstPick=== 4) {
+    else if (firstPick >= 18 && firstPick <= 23) {
       const chosenItem = pickItem(cost4);
-      document.getElementById("pokemon").textContent += chosenItem + " (4 Punkt)";
+      document.getElementById("pokemon").textContent += chosenItem + " (4 Punkte)";
       pointCounter += 4;
     }
 
-    else if (firstPick === 5) {
+    else if (firstPick >= 24 && firstPick <= 28) {
       const chosenItem = pickItem(cost5);
-      document.getElementById("pokemon").textContent += chosenItem + " (5 Punkt)";
+      document.getElementById("pokemon").textContent += chosenItem + " (5 Punkte)";
       pointCounter += 5;
     }
 
-    else if (firstPick === 6) {
+    else if (firstPick === 29) {
       const chosenItem = pickItem(cost6);
-      document.getElementById("pokemon").textContent += chosenItem + " (6 Punkt)";
+      document.getElementById("pokemon").textContent += chosenItem + " (6 Punkte)";
       pointCounter += 6;
     }
 
-    else if (firstPick === 7) {
+    else if (firstPick === 30) {
       const chosenItem = pickItem(cost8);
-      document.getElementById("pokemon").textContent += chosenItem + " (8 Punkt)";
+      document.getElementById("pokemon").textContent += chosenItem + " (8 Punkte)";
       pointCounter += 8;
     }
 
     }
+
+  document.getElementById("ersatz").textContent += pickItem(cost1) + " (1 Punkt) " + pickItem(cost2) + " (2 Punkte) " + pickItem(cost3) + " (3 Punkte)";
+
   }
 
 /*function myFunction() {
@@ -103,3 +129,5 @@ function chooseRandom(){
 
 
 }*/
+
+
