12903b0ffd464a167f92b7b2c40c07f50f226c9c
Patrandler <166053055+Patrandler@users.noreply.github.com>
2024-06-08 22:36:54 +0200

    Update style.js

--- style.js
+++ style.js
@@ -27,7 +27,8 @@ const cost5 = ["Sichlor", "Magmar", "Tauros", "Lapras", "Relaxo", "Skaraborn", "
 , "Mopex", "Nigiragi", "Gierspenst", "PalTauros" 
 ]
 const cost6 = ["Darkrai", "Cresselia", "Shaymin",  "Selfe", "Tobutz", "Kapu-Riki", "Masskito", "Chonjian", "Beatori", "Kriechflügel", "Eisenfalter"]
-const cost8 = ["Palkia", "Katagami (7 cost braucht eigene Liste)", "Demeteros(7 cost braucht eigene Liste)"]
+const cost8 = ["Palkia"]
+const cost7 = ["Katagami", "Demeteros"]
 
 
 let pointCounter = 0;
@@ -100,6 +101,75 @@ function myFunction() {
     }
 
   document.getElementById("ersatz").textContent += pickItem(cost1) + " (1 Punkt) " + pickItem(cost2) + " (2 Punkte) " + pickItem(cost3) + " (3 Punkte)";
+
+  }
+
+function myFunction2() {
+
+  while (pointCounter < 10) {
+
+    let firstPick = randomizer(21);
+    /*do {
+      firstPick = randomizer(8);
+      // Check for overflow with all possibilities (assuming pointCounter starts at 0)
+    } while ( (firstPick === 1 && pointCounter + 3 > 10) ||
+      (firstPick === 2 && pointCounter + 2 > 10) ||
+      (firstPick === 3 && pointCounter + 3 > 10) ||
+      (firstPick === 4 && pointCounter + 4 > 10) ||
+      (firstPick === 5 && pointCounter + 5 > 10) ||
+      (firstPick === 6 && pointCounter + 6 > 10) ||
+      (firstPick === 7 && pointCounter + 8 > 10)
+      // ... add checks for all other options (4, 5, 6, 7)
+      );*/
+
+    if ((firstPick >= 0 && firstPick <= 5) && pointCounter <= 7){
+      pointCounter += 3;
+      const chosenItem = pickItem(cost3);
+      document.getElementById("pokemon2").textContent += chosenItem + " (3 Punkte)";
+
+    } else if ((firstPick >= 6 && firstPick <= 7) && pointCounter <= 8){
+      const chosenItem = pickItem(cost2);
+      document.getElementById("pokemon2").textContent += chosenItem + " (2 Punkte)";
+      pointCounter += 2;
+    } else if (firstPick >= 9 && firstPick <= 9) {
+      const chosenItem = pickItem(cost1);
+      document.getElementById("pokemon2").textContent += chosenItem + " (1 Punkt)";
+      pointCounter += 1;
+    }
+    else if ((firstPick >= 11 && firstPick <= 14) && pointCounter <= 6) {
+      const chosenItem = pickItem(cost4);
+      document.getElementById("pokemon2").textContent += chosenItem + " (4 Punkte)";
+      pointCounter += 4;
+    }
+
+    else if ((firstPick >= 15 && firstPick <= 19) && pointCounter <= 5){
+      const chosenItem = pickItem(cost5);
+      document.getElementById("pokemon2").textContent += chosenItem + " (5 Punkte)";
+      pointCounter += 5;
+    }
+
+    else if ((firstPick === 20) && pointCounter <= 4){
+      const chosenItem = pickItem(cost6);
+      document.getElementById("pokemon2").textContent += chosenItem + " (6 Punkte)";
+      pointCounter += 6;
+    }
+
+    else if ((firstPick === 21) && pointCounter <=2) {
+      const chosenItem = pickItem(cost8);
+      document.getElementById("pokemon2").textContent += chosenItem + " (8 Punkte)";
+      pointCounter += 8;
+    } else if ((firstPick=== 22) && pointCounter <= 3) {
+      const chosenItem = pickItem(cost7);
+      document.getElementById("pokemon2").textContent += chosenItem + " (7 Punkte)";
+      pointCounter += 7;
+    }
+
+    else {
+      pointCounter += 0;
+    }
+    }
+
+  document.getElementById("ersatz2").textContent += pickItem(cost1) + " (1 Punkt) " + pickItem(cost2) + " (2 Punkte) " + pickItem(cost3) + " (3 Punkte)";
 
   }
 
